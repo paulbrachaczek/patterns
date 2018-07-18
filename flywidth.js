6ef22486e05144dde88bfb9a7e77f452efd7150c
Paweł Brachaczek <pawel.brachaczek@omnisquare.pl>
2018-07-18 16:01:08 +0200

    flywidth

---
+++ flywidth.js
@@ -0,0 +1,20 @@
+class Book {
+    constructor(title, author, iban, pages, year, gerne){
+        this.title = title;
+        this.author = author;
+        this.pages = pages
+        this.iban = iban,
+        this.year = year;
+        this.gerne = gerne;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+
+    showGerne(){
+        return this.gerne;
+    }
+}
+
+let it = new Book('it', 'stephen King', 12345, 700, 1990, 'horror');
